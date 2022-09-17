4ae7b2dc05b5f22dbfe46a4cf58e6508ace115e0
Wahyu440 <wahyu.vivacious@si.ukdw.ac.id>
2022-09-18 00:35:53 +0700

    tugas 2: Prevent Back From Activity

--- Mobile_1/app/src/main/java/com/example/mobile_1/MainActivity.kt
+++ Mobile_1/app/src/main/java/com/example/mobile_1/MainActivity.kt
@@ -1,58 +1,45 @@
 package com.example.mobile_1
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.mobile_1.databinding.ActivityMainBinding
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tvMain : TextView
+    lateinit var btnInputNama : Button
+    lateinit var edInputNama : EditText
+    lateinit var btnHelp : Button
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        tvMain = findViewById(R.id.tv_main_activity)
+        tvMain.text = getString(R.string.text_progmob_main)
 
-        setSupportActionBar(binding.toolbar)
+        btnInputNama = findViewById(R.id.btn_ambil_nama)
+        btnHelp = findViewById(R.id.btn_help)
+        edInputNama = findViewById(R.id.ed_input_nama)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        btnInputNama.setOnClickListener ( View.OnClickListener { view ->
+            var strTmp = edInputNama.text.toString()
+            tvMain.text = strTmp
+        } )
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
+        btnHelp.setOnClickListener ( View.OnClickListener { view ->
+            var bundle = Bundle()
+            var strTmp = edInputNama.text.toString()
+            bundle.putString("tesText",strTmp)
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+            var intent = Intent(this@MainActivity, HelpActivity::class.java)
+            intent.putExtras(bundle)
+            startActivity(intent)
+            finish()
+        } )
     }
 }
